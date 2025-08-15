7caecb060718d5cba616fe8b8d699e449ef7ceb9
aaaa1024 <suga.ryota.24@shizuoka.ac.jp>
2025-08-16 01:47:24 +0900

    ネスト許さない

--- Yaju/massageYaju.py
+++ Yaju/massageYaju.py
@@ -7,6 +7,27 @@ tree = app_commands.CommandTree(bot)
 
 TOKEN = "1"
 
+
+class QuestionView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=180)
+        self.ans = "Sure! "
+
+    @discord.ui.button(label="Sure! ", style=discord.ButtonStyle.success)
+    async def sure_button(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        # self.ans = "Sure! "
+        await interaction.response.edit_message(content=f"Yay!")
+
+    @discord.ui.button(label="nop. ", style=discord.ButtonStyle.danger)
+    async def nop_button(
+        self, interaction: discord.Interaction, button: discord.ui.button
+    ):
+        # self.ans = "nop. "
+        await interaction.response.edit_message(content=f"nooooooooooo")
+
+
 with open(".env", "r", encoding="utf-8") as f:
     for line in f:
         if line[0 : len("TOKEN=")] == "TOKEN=":
@@ -22,7 +43,7 @@ async def on_ready():
 @tree.command(name="icetea", description="サー")
 async def ice_tea(interaction: discord.Interaction):
     await interaction.response.send_message(
-        "お　ま　た　せ \n アイスティーしかなかったけど、いいかな？"
+        "お　ま　た　せ \nアイスティーしかなかったけど、いいかな？"
     )
 
 
@@ -41,6 +62,13 @@ async def massege_yaju(
         await interaction.response.send_message("素晴らしい!")
     else:
         await interaction.response.send_message("残念、その程度の人間なんだね")
+
+
+@tree.command(name="doyoulikeyaku", description="Do you like Yaju? ")
+async def doyoulikeyaju(interaction: discord.Interaction):
+    view = QuestionView()
+
+    await interaction.response.send_message("Do you like Yaju? ", view=view)
 
 
 bot.run(TOKEN)
